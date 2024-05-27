a1174f1a1810ed74a5d79142038d48391a1afe96
chakrawat <158020070+chakrawat@users.noreply.github.com>
2024-05-27 14:17:38 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,158 @@
+var tourpoint=
+{
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดสระเกศราชวรมหาวิหาร (กรุงเทพมหานคร)",
+          "type": "cutural",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            100.50662099774912,
+            13.754082531694399
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "อุทยานแห่งชาติป่าหินงาม (จังหวัดชัยภูมิ)",
+          "type": "spacial",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            101.39394574010397,
+            15.631869953303488
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "สวนสยาม",
+          "type": "spacial",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            100.6918241409429,
+            13.805273492807258
+            
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เกาะกูด",
+          "type": "natural",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+          102.56491091939453,
+          11.688593348710516, 
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ศูนย์เรียนรู้โลกใต้ทะเลบางแสน",
+          "type": "	spacial",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+           100.92491949586083,
+           13.28380586599407
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "สวนรถไฟ",
+          "type": "natural",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+           100.55660162853901,
+           13.81333515798717
+
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": " เกาะสีชัง",
+          "type": "natural",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            100.80852830249036,
+            13.149689972186358,
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดพระธาตุเขาน้อย",
+          "type": "cutural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+             100.75042046691969,
+             18.769904518182514,
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ถ้ำน้ำเขาศิวะ",
+          "type": "natural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+             102.32773166520874,
+             13.321075250305277,
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ภูทับเบิก",
+          "type": "natural",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+             101.08666413301303,
+             16.90751814199748
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
